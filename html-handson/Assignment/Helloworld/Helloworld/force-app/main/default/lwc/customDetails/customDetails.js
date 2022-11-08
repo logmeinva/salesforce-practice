321c8b64a262b1a8aff387822d0b99b9aa5cb728
PravallikaAytha <logmeinva@gmail.com>
2022-11-07 21:15:41 -0600

    Collected in relatedRecords - No compile errors.

--- html-handson/Assignment/Helloworld/Helloworld/force-app/main/default/lwc/customDetails/customDetails.js
+++ html-handson/Assignment/Helloworld/Helloworld/force-app/main/default/lwc/customDetails/customDetails.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , api, wire} from 'lwc';
+import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
 
-export default class CustomDetails extends LightningElement {}
+
+export default class CustomDetails extends LightningElement {
+    @api recordId;
+     @wire(getRelatedListRecords, {
+        parentRecordId: '0018X000037mM6BQAU',
+        relatedListId: 'Opportunity',
+        fields: ['Opportunity.StageName','Opportunity.Amount','Opportunity.CloseDate'],
+        sortBy:['Opportunity.StageName']
+    })
+    relatedRecord;
+}
